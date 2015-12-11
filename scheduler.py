8ec06de68be17f2ccea4a341c69ed20ac9e6c21d
mlimaloureiro <mdiasloureiro@gmail.com>
2015-12-11 11:46:22 +0000

    getting instances from tags

--- scheduler.py
+++ scheduler.py
@@ -6,9 +6,9 @@ Usage:
   scheduler --version
  
 Options:
-  -c --config CONFIG    Use alternate configuration file [default: ./aws.cnf].
-  -h --help             Show this screen.
-  --version             Show version.
+  -c --config CONFIG     Use alternate configuration file [default: ./aws.cnf].
+  -h --help              Show this screen.
+  --version              Show version.
 """
 
 from docopt import docopt
@@ -17,13 +17,14 @@ import boto.ec2
 import sys, os, json, datetime, time
 
 config = SafeConfigParser()
- 
-def init():
+ec2 = {}
+
+def init(args):
     # Setup AWS connection
-    global ec2_eu
-    global ec2_us
-    ec2_eu = connect_from_conf('aws_eu')
-    ec2_us = connect_from_conf('aws_us')
+    ec2['eu-west-1'] = connect_from_conf('aws_eu')
+    ec2['us-west-1'] = connect_from_conf('aws_us')
+    global schedules
+    schedules = get_schedules()
 
 def connect_from_conf(aws_conf):
     aws_access_key = config.get(aws_conf,'access_key','')
@@ -35,9 +36,26 @@ def connect_from_conf(aws_conf):
             aws_access_key_id = aws_access_key,
             aws_secret_access_key = aws_secret_key)
 
+def get_schedules():
+  path = config.get('schedule', 'paths', './schedule.json')
+  with open(path) as schedule_file:
+    return json.load(schedule_file)
+
+def schedule():
+  for profile in schedules['profiles']:
+    instances = get_instances(profile['instance_tags'], profile['region'])
+    print instances
+
+def get_instances(instance_tags, region):
+  instances = []
+  for tag in instance_tags:
+    instances.append(ec2[region].get_all_instances(filters={"tag:Name": instance_tags}))
+  return instances
+
 def run(args):
     config.read([args['--config'], 'aws.conf'])
-    init()
+    init(args)
+    schedule()
 
 if __name__ == "__main__":
     args = docopt(__doc__, version='scheduler 1.0')
